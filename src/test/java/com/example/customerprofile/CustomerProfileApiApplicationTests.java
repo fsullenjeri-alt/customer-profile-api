221ba0e9b47273a9a6ae41e9af8018018e125f3c
fsullenjeri-alt <fsullenjeri@protonmail.com>
2026-09-23 22:49:09 +0200

    Initial project setup

---
+++ src/test/java/com/example/customerprofile/CustomerProfileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customerprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerProfileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
